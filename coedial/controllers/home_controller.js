144729d1ab5f7d6c54f1ee9b856a62340b5c136d
Siddharth2808 <c.siddharth@iitg.ac.in>
2021-05-19 23:43:21 +0530

    Second Commit

--- coedial/controllers/home_controller.js
+++ coedial/controllers/home_controller.js
@@ -1,16 +1,32 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
 
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
-    Post.find({}).populate('user').exec( function(err,posts)
-        {
-            return res.render('home', {
-                title: "Post-List",post_list: posts
-            });
-        })
-    }
 
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts:  posts
+    //     });
+    // });
 
+    // populate the user of each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
+
+}
 
 // module.exports.actionName = function(req, res){}
