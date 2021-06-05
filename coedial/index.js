8154a16ec280b3bcfca62ea46d0f306cdc2bb5e5
Siddharth2808 <76950378+Siddharth2808@users.noreply.github.com>
2021-06-05 22:49:28 +0530

    Added Chat option

--- coedial/index.js
+++ coedial/index.js
@@ -8,10 +8,19 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+// const passportJWT = require('./config/passport-jwt-strategy');
+// const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
+
+// setup the chat server to be used with socket.io
+const chatServer = require('http').createServer(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is listening on port 5000');
 
 
 app.use(sassMiddleware({
@@ -26,7 +35,8 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
-app.use('/uploads', express.static(__dirname+'/uploads'));
+// make the uploads path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
